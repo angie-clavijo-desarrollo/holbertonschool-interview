681a13651899f00ea6b83f1f3534a61829e774b4
AnguiClvijoGutierrez <angui.clavijo@imagineapps.co>
2022-04-24 20:00:21 -0500

    question interview

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,8 +4,9 @@ def canUnlockAll(boxes):
         if not intoBox:
             continue
         for keyValue in intoBox:
-            if keyValue < len(boxes) and keyValue not in boxUnlocked and keyValue != boxIndex:
-                boxUnlocked.append(keyValue)
+            if keyValue < len(boxes) and keyValue not in boxUnlocked:
+                if keyValue != boxIndex:
+                    boxUnlocked.append(keyValue)
     if len(boxUnlocked) == len(boxes):
         return True
     return False
