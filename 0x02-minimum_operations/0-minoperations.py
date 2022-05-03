91ab5d33e63e0e488adfd2e4b067fec2218aca0c
AnguiClvijoGutierrez <angui.clavijo@imagineapps.co>
2022-05-02 21:14:15 -0500

    lock boxes

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single character H. Your text editor can execute only two operations 
+in this file: Copy All and Paste. Given a number n, write a method that calculates the fewest 
+number of operations needed to result in exactly n H characters in the file.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Verify if box is blocked
+    """
+
+    if boxes is None or len(boxes) == 0:
+        return False
+
+    unlocked = []
+    keys = []
+    for i in boxes:
+        unlocked.append(0)
+
+    unlocked[0] = 1
+    keys.append(0)
+    while keys:
+        key = keys.pop(0)
+        new_keys = boxes[key]
+        unlocked[key] = 1
+        for new_key in new_keys:
+            if int(new_key) < len(boxes) and unlocked[new_key] == 0:
+                keys.append(new_key)
+                unlocked[new_key] = 1
+
+    if 0 in unlocked:
+        return False
+
+    return True
